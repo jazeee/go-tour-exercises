37b2b39b3701555f0b6a81770f99bd46320a964c
Jaz Singh <jsingh@practichem.com>
2016-10-31 13:31:58 -0700

    Solution from https://tour.golang.org/concurrency/8

---
+++ tree/binary/equivalent.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, rootTree *tree.Tree, ch chan int) {
+	if t != nil {
+		Walk(t.Left, rootTree, ch)
+		ch <- t.Value
+		Walk(t.Right, rootTree, ch)
+	}
+	if t == rootTree {
+		close(ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, t1, ch1)
+	go Walk(t2, t2, ch2)
+	for v1 := range ch1{
+		v2 := <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	t := tree.New(1)
+	go Walk(t, t, ch)
+	for val := range ch {
+		fmt.Println(val)
+	}
+	fmt.Println(Same(tree.New(3), tree.New(2)))
+	fmt.Println(Same(tree.New(2), tree.New(2)))
+}
+
